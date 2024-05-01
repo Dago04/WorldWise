ba9c049459b47e80e9bcef9b90d1cb8dd61233f2
Dagoberto Salas <DagoSalas1999@gmail.com>
2024-05-01 11:55:36 -0600

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-function App() {
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Product } from './pages/Product'
+import { Pricing } from './pages/Pricing'
+import { Homepage } from './pages/Homepage'
+export const App = () => {
   return (
-    <div>
-      Worldwise
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='product' element={<Product />} />
+        <Route path='pricing' element={<Pricing />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+
